93a31e2a4b94bb6fd94a085b05b1794dffc74230
igorsousap <igorsousapinto140@gmail.com>
2025-05-12 21:14:35 -0300

    change test

--- config/test.exs
+++ config/test.exs
@@ -5,10 +5,16 @@ import Config
 # The MIX_TEST_PARTITION environment variable can be used
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
+hostname =
+  case System.get_env("GITHUB_ACTIONS") do
+    "true" -> "localhost"
+    _ -> "postgres"
+  end
+
 config :management_api, ManagementApi.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "postgres",
+  hostname: hostname,
   database: "management_api_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
